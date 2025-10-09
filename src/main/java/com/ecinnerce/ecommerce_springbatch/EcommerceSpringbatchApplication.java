f304a674d762a5a23fb04b3158c5001e721394b8
postwo <codo7717@naver.com>
2025-10-09 13:14:34 +0900

    기본 세팅

---
+++ src/main/java/com/ecinnerce/ecommerce_springbatch/EcommerceSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.ecinnerce.ecommerce_springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringbatchApplication.class, args);
+	}
+
+}
